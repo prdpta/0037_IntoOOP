7a8eed0ed13cfd2879fc8faf962d246f44f3a55a
prdpta <pradiptaputramei01@gmail.com>
2024-04-23 17:06:33 +0700

    menambahkan cout nama umur jurusan

--- 0037_IntoOOP.cpp
+++ 0037_IntoOOP.cpp
@@ -6,3 +6,11 @@ class Mahasiswa{
         string nama;
         int umur;
         string jurusan;
+
+        void output(){
+            cout << "Nama: " << nama << endl;
+            cout << "Umur: " << umur << endl;
+            cout << "Jurusan: " << jurusan << endl;
+        }
+
+};
